197e7cd1a5c17833906f152297ec50ff7d9a8e42
muhammad saleet <muhammad-saleet@hotmail.com>
2020-07-16 23:27:59 +0300

    Updates

---
+++ precache-manifest.a3ae9783952c0dfdde965a00e41f9b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808abd615e9851e41d9ea210f1816960",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "7fb3f76d37441927e344",
+    "url": "/books/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fb3f76d37441927e344",
+    "url": "/books/static/js/2.69138d8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/books/static/js/2.69138d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ef8a2e1278e2231d34",
+    "url": "/books/static/js/main.9f39bee0.chunk.js"
+  },
+  {
+    "revision": "1c3c72831bb03b6ab714",
+    "url": "/books/static/js/runtime-main.462624b3.js"
+  }
+]);
